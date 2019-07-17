c04439e934be8db990335b55a5f34328bbf94868
jiangxiaoyao <jiangxiaoyao007@gmail.com>
2019-07-17 22:29:21 +0800

    设置cookie和session

--- src/main/java/com/etc/controller/UserController.java
+++ src/main/java/com/etc/controller/UserController.java
@@ -4,12 +4,18 @@ import com.etc.vo.LoginReq;
 import com.etc.vo.LoginResp;
 import jdk.nashorn.internal.ir.RuntimeNode;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
+@SessionAttributes(value = "loginUser")
 public class UserController {
+
     @RequestMapping("/login.do")
     @ResponseBody
     public LoginResp login(@RequestBody LoginReq loginReq){
@@ -24,5 +30,32 @@ public class UserController {
         }
         return loginResp;
     }
+
+    @RequestMapping("/login2.do")
+    @ResponseBody
+//    设置session方式一(生命周期：浏览器窗口)
+//    public LoginResp login2(String username, HttpSession session){
+//        session.setAttribute("loginUser",username);
+//        return null;
+//    }
+//    设置session方式二
+    public LoginResp login2( String username, Model model){
+        model.addAttribute("loginUser",username);
+        return null;
+    }
+
+    //    设置cookie
+    @RequestMapping("/cookie")
+    public String cookie(HttpServletResponse response){
+        Cookie cookie = new Cookie("name", "jiang");
+        response.addCookie(cookie);
+        return null;
+    }
+    @RequestMapping("/getcookie")
+    public String getcookie(@CookieValue("name") String name){
+        System.out.println("name:"+name);
+        return null;
+    }
+
 
 }
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jiangxiaoyao
+  Date: 2019/7/17
+  Time: 21:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="login2.do">
+    username:<input name="username"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
