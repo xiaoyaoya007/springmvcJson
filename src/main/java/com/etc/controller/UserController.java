f68fe25b82f3b86fe2fb07f7c6f6b24647b50649
xiaoyaoya007 <45869562+xiaoyaoya007@users.noreply.github.com>
2019-07-18 08:38:04 +0800

    修改

--- src/main/java/com/etc/controller/UserController.java
+++ src/main/java/com/etc/controller/UserController.java
@@ -6,15 +6,24 @@ import jdk.nashorn.internal.ir.RuntimeNode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SessionAttributes(value = "loginUser")
 public class UserController {
+
+    @RequestMapping("/user/{id}")//路径参数
+    public String query(@PathVariable("id") int id){
+        System.out.println(id);
+        return null;
+    }
 
     @RequestMapping("/login.do")
     @ResponseBody
